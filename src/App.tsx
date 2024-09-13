6535d110b6237e07192a25e026c557d9efb5ea5e
Md. Siam Shekh <siam62349@gmail.com>
2024-09-13 19:27:21 +0600

    Navbar complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { CgArrowTopRight } from 'react-icons/cg';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-black">
+      <div className='min-h-screen mx-auto max-w-[1200px]'>
+        <div className="navbar ">
+          <div className="navbar-start">
+            {/* Only for small device */}
+            <div className="dropdown">
+              <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor">
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16" />
+                </svg>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                <li><a>Item 1</a></li>
+                <li>
+                  <a>Parent</a>
+                  <ul className="p-2">
+                    <li><a>Submenu 1</a></li>
+                    <li><a>Submenu 2</a></li>
+                  </ul>
+                </li>
+                <li><a>Item 3</a></li>
+              </ul>
+            </div>
+            <a className="text-xl">
+              <p className='text-center font-inter text-white text-xl'>
+                <span className='block'>J.</span>
+                <span className='block'>Joseph</span>
+                <span className='block'>Nahid</span>
+              </p>
+            </a>
+          </div>
+          {/* Only for large device */}
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1 gap-4 uppercase font-inter text-xl text-white">
+              <li>About me</li>
+              <li>Skills</li>
+              <li>Projects</li>
+            </ul>
+          </div>
+          <div className="navbar-end">
+           <div className="flex justify-between items-center gap-2 border border-white rounded-full pl-3 pr-1 py-1">
+            <p className='uppercase text-xl font-inter text-white'>Contact</p>
+            <CgArrowTopRight className='text-3xl bg-[#FF00E5] text-black rounded-full'/>
+           </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
